5ed878ae021496701855f8f81a56a74dccb2b01d
drdevanshu <56801218+drdevanshu@users.noreply.github.com>
2019-11-06 22:29:01 -0500

    changes

--- src/main/java/com/busyqa/coursework/wk1_answer/CheckPassFail.java
+++ src/main/java/com/busyqa/coursework/wk1_answer/CheckPassFail.java
@@ -2,10 +2,10 @@ package com.busyqa.coursework.wk1_answer;
 
 public class CheckPassFail {  // Save as "CheckPassFail.java"
     public static void main(String[] args) {  // Program entry point
-        int mark = 49;  // Set the value of "mark" here!
+        int mark = 52;  // Set the value of "mark" here!
         System.out.println("The mark is " + mark);
 
-        if ( mark>=50 ) {
+        if ( mark>=51 ) {
             System.out.println( "pass" );
        } else {
            System.out.println("fail");
