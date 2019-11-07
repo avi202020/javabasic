ec341bad46838b0fe7ef0926dfc517919bec19c2
drdevanshu <56801218+drdevanshu@users.noreply.github.com>
2019-11-06 22:37:38 -0500

    newchange

--- src/main/java/com/busyqa/coursework/wk1_answer/CheckOddEven.java
+++ src/main/java/com/busyqa/coursework/wk1_answer/CheckOddEven.java
@@ -2,13 +2,14 @@ package com.busyqa.coursework.wk1_answer;
 
 public class CheckOddEven  {
     public static void main(String[]  args) { // Program entry point
-    	int number = 50;       // Set the value of "number" here!
+    	int number = 51;       // Set the value of "number" here!
     	System.out.println("The number is " + number);
-    	if ( number == 50 ) {
-    		System.out.println( "even" );
-    	} else {
+    	if ( number >= 50 ) {
     		System.out.println( "odd" );
+    	} else {
+    		System.out.println( "even" );
     	}
     	System.out.println( "done" );
+//    	new change
     }
 }
